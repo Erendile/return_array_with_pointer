ea7d5eec26410dce7cdc8c338175265d8280634a
Erendile <74563063+Erendile@users.noreply.github.com>
2021-01-10 11:51:01 +0300

    Create math_operations.c

---
+++ math_operations.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define    COUNT    4
+char* operations[COUNT] = {"x + y = ","x - y = ","x * y = ","x / y = "};
+
+int* func(int* x, int* y);
+
+int main()
+{
+    int x = 10,  y = 5;
+    for(int i = 0; i < COUNT; i++)
+        printf("%s%d\n", operations[i], func(&x, &y)[i]);
+
+    return 0;
+}
+
+int* func(int* x, int* y)
+{
+    static int results[COUNT];
+    for(int i = 0; i < COUNT; i++)
+    {
+        if(i == 0)
+            results[i] = *x + *y;
+        if(i == 1)
+            results[i] = *x - *y;
+        if(i == 2)
+            results[i] = *x * *y;
+        if(i == 3)
+            results[i] = *x / *y;
+    }
+    return results;
+}
